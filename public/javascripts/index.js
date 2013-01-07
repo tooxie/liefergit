8c79da596013139c50244d7eed1bc99a708fb936
Tim Beyer <tim.beyer@deliveryhero.com>
2013-01-07 13:42:50 +0100

    Refactor pull request creation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -104,7 +104,33 @@ window.liefergit = (function (module, $) {
 
     var SubmoduleReferenceUpdater = function (submoduleRepo, fromRepo, toRepo) {
 
-        this.createPullRequest = function (submoduleRef, fromRepoBranchName) {
+        var createUpdatedTree = function (fromRepoCommits, repoSha) {
+            var repoPath = submoduleRepo.get("path");
+            var theTree = [{
+              "path": repoPath,
+              "mode": "160000", //one of 100644 for file (blob), 100755 for executable (blob), 040000 for subdirectory (tree), 160000 for submodule (commit) or 120000 for a blob that specifies the path of a symlink
+              "type": "commit",
+              "sha": repoSha
+            }];
+            var fromRepoHeadTreeSha = _.first(fromRepoCommits).commit.tree.sha;
+
+            return fromRepo.postTreeFromBase(theTree, fromRepoHeadTreeSha);
+        }
+
+        var createPullRequest = function (fromRepoBranchName) {
+            var prTitle = "Updating submodule";
+            var originRepoUser = fromRepo.get("user");
+
+            var prData = {
+                "title": prTitle,
+                "body": "A nice pull request",
+                "head": originRepoUser+":"+fromRepoBranchName,
+                "base": "master"
+            }
+            return toRepo.createPullRequest(prData);
+        }
+
+        this.createUpdatePullRequest = function (submoduleRef, fromRepoBranchName) {
 
             var successDeferred = $.Deferred();
 
@@ -114,6 +140,7 @@ window.liefergit = (function (module, $) {
             $.when( fromRepoCommitsDeferred, submoduleRefDeferred )
             .then(function (fromRepoCommits, repoSha) {
 
+                
                 var repoName = submoduleRepo.get("name");
                 var repoPath = submoduleRepo.get("path");
                 var commitMsg = "Updated submodule " + repoName;
@@ -122,20 +149,14 @@ window.liefergit = (function (module, $) {
                 //the branch which will be used to issue the PR's against upstream
                 //var subrepoBranchName = "master";
 
-                var originRepoUser = fromRepo.get("user");
 
                 var fromRepoHeadSha = _.first(fromRepoCommits).sha;
                 var fromRepoHeadTreeSha = _.first(fromRepoCommits).commit.tree.sha;
 
-                var theTree = [{
-                  "path": repoPath,
-                  "mode": "160000", //one of 100644 for file (blob), 100755 for executable (blob), 040000 for subdirectory (tree), 160000 for submodule (commit) or 120000 for a blob that specifies the path of a symlink
-                  "type": "commit",
-                  "sha": repoSha
-                }];
 
+                var updatedTreeDeferred = createUpdatedTree(fromRepoCommits, repoSha);
 
-                fromRepo.postTreeFromBase(theTree, fromRepoHeadTreeSha)
+                $.when(updatedTreeDeferred)
                 .then(function(updateTreeSha){
                     var commitMessage = "Updating repository " + repoName;
 
@@ -144,14 +165,8 @@ window.liefergit = (function (module, $) {
 
                         fromRepo.updateHead(fromRepoBranchName, updateCommitSha)
                         .then(function(){
-                            var prTitle = commitMessage;
-                            var prData = {
-                                "title": prTitle,
-                                "body": "A nice pull request",
-                                "head": originRepoUser+":"+fromRepoBranchName,
-                                "base": "master"
-                            }
-                            toRepo.createPullRequest(prData)
+
+                            createPullRequest(fromRepoBranchName)
                             .then(function(res){
                                 successDeferred.resolve();
                             });
@@ -196,7 +211,7 @@ window.liefergit = (function (module, $) {
                 this.state.isUpdating = true;
 
                 var updater = new SubmoduleReferenceUpdater(this.submoduleRepo, this.originRepo, this.upstreamRepo);
-                var prSuccess = updater.createPullRequest("heads/master", "master");
+                var prSuccess = updater.createUpdatePullRequest("heads/master", "master");
                 $.when(prSuccess).then(function () {
                     self.state.pullRequestSent = true;
                     self.render();
